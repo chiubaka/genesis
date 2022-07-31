c954798ddb745efe0e985bff929c4ffc8cd32e49
Daniel <1732408+chiubaka@users.noreply.github.com>
2022-07-31 16:04:02 +0800

    Set up basic Git hooks with `husky` and `lint-staged` (#26)
    * Add husky and basic git hooks
    
    * Set up lint-staged on pre-commit
    
    * Expand linting scope to all files
    
    * Fix remaining linting issues
    
    * Fix e2e tests
    
    * Run both testing commands pre-push regardless of failure

--- e2e/nx-plugin-e2e/jest.config.ts
+++ e2e/nx-plugin-e2e/jest.config.ts
@@ -1,15 +1,14 @@
-/* eslint-disable */
 export default {
-  displayName: 'nx-plugin-e2e',
-  preset: '../../jest.preset.js',
+  displayName: "nx-plugin-e2e",
+  preset: "../../jest.preset.js",
   globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
     },
   },
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    "^.+\\.[tj]s$": "ts-jest",
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/e2e/nx-plugin-e2e',
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/e2e/nx-plugin-e2e",
 };
