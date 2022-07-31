fb72fd3b472d35d2618d95220261e961368014b3
Daniel Chiu <daniel@chiubaka.com>
2022-08-01 06:59:03 +0800

    Fix linting errors

--- e2e/nx-plugin-e2e/tests/preset.spec.ts
+++ e2e/nx-plugin-e2e/tests/preset.spec.ts
@@ -1,13 +1,11 @@
 import {
   checkFilesExist,
   ensureNxProject,
-  exists,
-  readJson,
   runNxCommandAsync,
   uniq,
-} from '@nrwl/nx-plugin/testing';
+} from "@nrwl/nx-plugin/testing";
 
-describe('nx-plugin e2e', () => {
+describe("nx-plugin e2e", () => {
   // Setting up individual workspaces per
   // test can cause e2e runs to take a long time.
   // For this reason, we recommend each suite only
@@ -15,41 +13,39 @@ describe('nx-plugin e2e', () => {
   // on a unique project in the workspace, such that they
   // are not dependant on one another.
   beforeAll(() => {
-    ensureNxProject('@chiubaka/nx-plugin', 'dist/packages/nx-plugin');
+    ensureNxProject("@chiubaka/nx-plugin", "dist/packages/nx-plugin");
   });
 
-  afterAll(() => {
+  afterAll(async () => {
     // `nx reset` kills the daemon, and performs
     // some work which can help clean up e2e leftovers
-    runNxCommandAsync('reset');
+    await runNxCommandAsync("reset");
   });
 
-  describe('with default options', () => {
+  describe("with default options", () => {
     let project: string;
 
     beforeAll(async () => {
-      project = uniq('preset');
-      await runNxCommandAsync(
-        `generate @chiubaka/nx-plugin:preset ${project}`
-      );
+      project = uniq("preset");
+      await runNxCommandAsync(`generate @chiubaka/nx-plugin:preset ${project}`);
     });
 
-    it('should not create an apps dir', async () => {
+    it("should not create an apps dir", () => {
       expect(() => {
-        checkFilesExist('apps');
+        checkFilesExist("apps");
       }).toThrow();
     });
 
-    describe('package manager', () => {
-      it('should install packages with yarn', () => {
+    describe("package manager", () => {
+      it("should install packages with yarn", () => {
         expect(() => {
-          checkFilesExist('yarn.lock');
+          checkFilesExist("yarn.lock");
         }).not.toThrow();
       });
 
-      it('should not install packages with npm', () => {
+      it("should not install packages with npm", () => {
         expect(() => {
-          checkFilesExist('package-lock.json');
+          checkFilesExist("package-lock.json");
         }).toThrow();
       });
     });
