eb5d8a19437b0dcacdffe2bc4a60d53859fd17a8
Daniel <1732408+chiubaka@users.noreply.github.com>
2022-07-31 14:49:28 +0800

    Extend @chiubaka/eslint-config for basic project linting (#18)
    * Extend @chiubaka/eslint-config and resolve basic linting issues
    
    * Update .eslintrc.json configurations
    
    * Upgrade to @chiubaka/eslint-config@0.3.2
    
    * Resolve linting issues with nx-plugin-checks

--- packages/nx-plugin/src/executors/build/executor.ts
+++ packages/nx-plugin/src/executors/build/executor.ts
@@ -1,8 +1,9 @@
-import { BuildExecutorSchema } from './schema';
+import { BuildExecutorSchema } from "./schema";
 
-export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build', options);
-  return {
+export default function runExecutor(options: BuildExecutorSchema) {
+  // eslint-disable-next-line no-console
+  console.log("Executor ran for Build", options);
+  return Promise.resolve({
     success: true,
-  };
+  });
 }
