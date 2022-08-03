b07a23344556224b06d9e1a255b3b470d5fef972
Daniel <1732408+chiubaka@users.noreply.github.com>
2022-08-03 08:00:05 +0800

    Create a faithful testing harness for workspace preset generation using Verdaccio (#35)
    * Create a faithful testing harness for workspace preset generation using Verdaccio
    
    * Ensure that destination directory exists in preset e2e tests
    
    * Double Jest test timeout for preset e2e tests

--- e2e/nx-plugin-e2e/tests/preset.spec.ts
+++ e2e/nx-plugin-e2e/tests/preset.spec.ts
@@ -1,93 +1,140 @@
 import {
   checkFilesExist,
-  ensureNxProject,
+  cleanup,
   runCommandAsync,
   runNxCommandAsync,
   tmpProjPath,
   uniq,
 } from "@nrwl/nx-plugin/testing";
+import { ensureDirSync } from "fs-extra";
+import { ChildProcess, execSync, fork } from "node:child_process";
 import fs from "node:fs";
+import path from "node:path";
+import { getPortPromise as getOpenPort } from "portfinder";
 
-jest.setTimeout(30_000);
+jest.setTimeout(10_000);
+
+const startVerdaccio = async (port: number): Promise<ChildProcess> => {
+  const configPath = path.join(__dirname, "../verdaccio.yml");
+  return new Promise((resolve, reject) => {
+    const child = fork(require.resolve("verdaccio/bin/verdaccio"), [
+      "-c",
+      configPath,
+      "-l",
+      `${port}`,
+    ]);
+
+    child.on("message", (message: { verdaccio_started: boolean }) => {
+      if (message.verdaccio_started) {
+        resolve(child);
+      }
+    });
+    child.on("error", (error: any) => reject([error]));
+    child.on("disconnect", (error: any) => reject([error]));
+  });
+};
 
 describe("nx-plugin e2e", () => {
+  let verdaccioProcess: ChildProcess;
+
   // Setting up individual workspaces per
   // test can cause e2e runs to take a long time.
   // For this reason, we recommend each suite only
   // consumes 1 workspace. The tests should each operate
   // on a unique project in the workspace, such that they
   // are not dependant on one another.
-  beforeAll(() => {
-    ensureNxProject("@chiubaka/nx-plugin", "dist/packages/nx-plugin");
+  beforeAll(async () => {
+    cleanup();
+
+    const verdaccioPort = await getOpenPort();
+    verdaccioProcess = await startVerdaccio(verdaccioPort);
+
+    const verdaccioUrl = `http://localhost:${verdaccioPort}`;
+
+    execSync(`yarn config set registry ${verdaccioUrl}`);
+
+    execSync(
+      `npx npm-cli-login -u chiubaka -p test -e test@chiubaka.com -r ${verdaccioUrl}`,
+    );
+    execSync(`npm publish --registry=${verdaccioUrl}`, {
+      cwd: path.join(__dirname, "../../../dist/packages/nx-plugin"),
+    });
+
+    const destination = path.join(tmpProjPath(), "..");
+    const workspaceName = path.basename(tmpProjPath());
+
+    ensureDirSync(destination);
+
+    execSync(
+      `npm_config_registry=${verdaccioUrl} npx create-nx-workspace ${workspaceName} --preset=@chiubaka/nx-plugin --nxCloud=false`,
+      {
+        cwd: destination,
+      },
+    );
   });
 
   afterAll(async () => {
     // `nx reset` kills the daemon, and performs
     // some work which can help clean up e2e leftovers
     await runNxCommandAsync("reset");
+
+    execSync(`yarn config set registry https://registry.yarnpkg.com`);
+
+    verdaccioProcess.kill();
   });
 
-  describe("with default options", () => {
-    let project: string;
+  it("should not create an apps dir", () => {
+    expect(() => {
+      checkFilesExist("apps");
+    }).toThrow();
+  });
 
-    beforeAll(async () => {
-      project = uniq("preset");
-      await runNxCommandAsync(`generate @chiubaka/nx-plugin:preset ${project}`);
+  describe("package manager", () => {
+    it("should install packages with yarn", () => {
+      expect(() => {
+        checkFilesExist("yarn.lock");
+      }).not.toThrow();
     });
 
-    it("should not create an apps dir", () => {
+    it("should not install packages with npm", () => {
       expect(() => {
-        checkFilesExist("apps");
+        checkFilesExist("package-lock.json");
       }).toThrow();
     });
+  });
 
-    describe("package manager", () => {
-      it("should install packages with yarn", () => {
-        expect(() => {
-          checkFilesExist("yarn.lock");
-        }).not.toThrow();
-      });
-
-      it("should not install packages with npm", () => {
-        expect(() => {
-          checkFilesExist("package-lock.json");
-        }).toThrow();
-      });
+  describe("linting", () => {
+    it("creates a working linting setup", () => {
+      expect(async () => {
+        await runCommandAsync("yarn run eslint --print-config package.json");
+      }).not.toThrow();
     });
 
-    describe("linting", () => {
-      it("creates a working linting setup", () => {
-        expect(async () => {
-          await runCommandAsync("yarn run eslint --print-config package.json");
-        }).not.toThrow();
-      });
+    it("creates a working lint fix setup", async () => {
+      const lintErrorsFileName = `${uniq("fixableLintErrors")}.ts`;
+      // eslint-disable-next-line security/detect-non-literal-fs-filename
+      fs.writeFileSync(
+        tmpProjPath(lintErrorsFileName),
+        "export const hello = 'Hello, world!'",
+      );
 
-      it("creates a working lint fix setup", async () => {
-        const lintErrorsFileName = `${uniq("fixableLintErrors")}.ts`;
-        // eslint-disable-next-line security/detect-non-literal-fs-filename
-        fs.writeFileSync(
-          tmpProjPath(lintErrorsFileName),
-          "export const hello = 'Hello, world!'",
-        );
+      await expect(async () => {
+        await runCommandAsync("yarn run eslint .");
+      }).rejects.toThrow();
 
-        await expect(async () => {
-          await runCommandAsync("yarn run eslint .");
-        }).rejects.toThrow();
+      await runCommandAsync("yarn run eslint --fix .");
 
-        await runCommandAsync("yarn run eslint --fix .");
+      expect(async () => {
+        await runCommandAsync("yarn run eslint .");
+      }).not.toThrow();
 
-        expect(async () => {
-          await runCommandAsync("yarn run eslint .");
-        }).not.toThrow();
+      fs.rmSync(tmpProjPath(lintErrorsFileName));
+    });
 
-        fs.rmSync(tmpProjPath(lintErrorsFileName));
-      });
-
-      it("generates a project without linting issues", () => {
-        expect(async () => {
-          await runCommandAsync("yarn run eslint .");
-        }).not.toThrow();
-      });
+    it("generates a project without linting issues", () => {
+      expect(async () => {
+        await runCommandAsync("yarn run eslint .");
+      }).not.toThrow();
     });
   });
 });
