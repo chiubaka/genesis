2089a62790fedb52753b3753c1c1555a732bd2f4
Daniel <1732408+chiubaka@users.noreply.github.com>
2022-08-10 12:21:53 +0800

    Set up Codecov for this repo itself (#49)
    * Ensure coverage reports are output by test:ci command
    
    * Upgrade to @chiubaka/circleci-orb@0.6.0
    
    * Revert "Upgrade to @chiubaka/circleci-orb@0.6.0"
    
    This reverts commit 666b6b18cf69a880e5adb6bfb2f63987712a678d.
    
    * Re-upgrade to @chiubaka/circleci-orb@0.6.0 now that orb version has published
    
    * Upgrade to @chiubaka/circleci-orb@0.6.1
    
    * Ensure all typical coverage report types are generated
    
    * Add a basic Codecov configuration

--- packages/nx-plugin/jest.config.ts
+++ packages/nx-plugin/jest.config.ts
@@ -12,4 +12,5 @@ export default {
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory: "../../coverage/packages/nx-plugin",
+  coverageReporters: ["clover", "json", "lcov", "text"],
 };
