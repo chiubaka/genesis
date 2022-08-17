3123169a1c10678c4922489224542a2efcbaf138
Daniel Chiu <daniel@chiubaka.com>
2022-08-17 10:33:26 +0800

    Mock out latest package version retrieval in unit tests

--- packages/nx-plugin/test/setup/jest.setup.ts
+++ packages/nx-plugin/test/setup/jest.setup.ts
@@ -1,3 +1,11 @@
 import { matchers } from "../matchers";
 
 expect.extend(matchers);
+
+jest.mock("../../src/utils/getLatestPackageVersion", () => {
+  return {
+    getLatestPackageVersion: () => {
+      return Promise.resolve("0.0.1");
+    },
+  };
+});
