52e3b4f6c3a2d9fe721cf9813203fa85d8219877
Daniel Chiu <daniel@chiubaka.com>
2022-08-17 08:38:10 +0800

    Add a basic README generator

--- packages/nx-plugin/src/generators/index.ts
+++ packages/nx-plugin/src/generators/index.ts
@@ -2,5 +2,6 @@ export * from "./ci";
 export * from "./git";
 export * from "./linting";
 export * from "./preset";
+export * from "./readme";
 export * from "./testing";
 export * from "./tsconfig";
