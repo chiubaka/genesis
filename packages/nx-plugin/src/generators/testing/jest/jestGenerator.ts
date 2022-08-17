d838a77f96146948f37c825e3440b856191df417
Daniel Chiu <daniel@chiubaka.com>
2022-08-17 16:49:37 +0800

    Add a basic jest generator

---
+++ packages/nx-plugin/src/generators/testing/jest/jestGenerator.ts
@@ -0,0 +1,38 @@
+import { generateFiles, Tree } from "@nrwl/devkit";
+import path from "node:path";
+
+import { generatorLogger as logger } from "../../../logger";
+import { addDependenciesToPackageJson } from "../../../utils";
+
+export async function jestGenerator(tree: Tree) {
+  logger.info("Generating Jest setup");
+
+  const installTask = await installDependencies(tree);
+  copyConfigTemplates(tree);
+
+  return async () => {
+    logger.info("Running post-processing tasks for Jest generator");
+
+    await installTask();
+  };
+}
+
+async function installDependencies(tree: Tree) {
+  const installTask = await addDependenciesToPackageJson(
+    tree,
+    [],
+    ["@nrwl/jest", "jest"],
+  );
+
+  return async () => {
+    logger.info("Installing new dependencies for Jest generator");
+
+    await installTask();
+  };
+}
+
+function copyConfigTemplates(tree: Tree) {
+  logger.info("Copying Jest configuration templates");
+
+  generateFiles(tree, path.join(__dirname, "./files"), ".", {});
+}
