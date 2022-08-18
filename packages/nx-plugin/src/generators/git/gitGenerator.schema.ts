31ed67a986f65a09de7b0f0e5ddec830df8384bb
Daniel Chiu <daniel@chiubaka.com>
2022-08-18 10:58:17 +0800

    Add github generator options to git generator but don't do anything with them yet

--- packages/nx-plugin/src/generators/git/gitGenerator.schema.ts
+++ packages/nx-plugin/src/generators/git/gitGenerator.schema.ts
@@ -1,9 +1,13 @@
 import { GitHooksGeneratorSchema } from "./gitHooks";
+import { GitHubGeneratorSchema } from "./github";
 
-export interface GitGeneratorSchema extends GitHooksGeneratorSchema {
+export interface GitGeneratorSchema
+  extends GitHooksGeneratorSchema,
+    Partial<GitHubGeneratorSchema> {
   commitMessage: string;
   committerEmail?: string;
   committerName?: string;
 
   skipGitHooks?: boolean;
+  skipGitHub?: boolean;
 }
