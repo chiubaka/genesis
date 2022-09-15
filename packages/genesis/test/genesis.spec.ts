5fa2d775ada9c2b750a8bfb71d6f4be583fcc82c
Daniel Chiu <daniel@chiubaka.com>
2022-09-15 11:55:03 +0800

    Skip git generation provided by create-nx-workspace in favor of git generation included in preset generator

--- packages/genesis/test/genesis.spec.ts
+++ packages/genesis/test/genesis.spec.ts
@@ -31,6 +31,7 @@ describe("genesis", () => {
         "--directory=genesis",
         "--workspaceName=genesis",
         "--workspaceScope=chiubaka",
+        "--skipGit",
         "--skipGitHub",
         "--registry=http://localhost:4873",
         "--disableImmutableInstalls=true",
