552bb2c2a81339583da40e0b109fde3682d73813
Daniel Chiu <daniel@chiubaka.com>
2023-10-21 10:41:40 +0800

    Fill in Apple developer team ID for E2E tests so that Apple credentials aren't required for CI E2E

--- e2e/genesis-e2e/utils/createReactNativeAppTemplate.ts
+++ e2e/genesis-e2e/utils/createReactNativeAppTemplate.ts
@@ -4,7 +4,7 @@ export async function createReactNativeAppTemplate(workspaceName: string) {
   const workspace = await copyWorkspaceTemplate(workspaceName);
 
   await workspace.execNx(
-    'generate @chiubaka/nx-plugin:app.react-native --name=react-native-app --appName="Genesis React Native App" --appId="com.chiubaka.genesis.example.ReactNativeApp" --appleId="daniel@chiubaka.com" --androidEmulatorAvdName="Detox" --iosCodeSigningGitRepositoryUrl="git@github.com:chiubaka/ios-code-signing.git" --appStoreConnectKeyIssuerId="1ced7937-7d1e-4874-9bc9-823c1825148f" --appStoreConnectKeyId="43YKVHYVJQ" --androidUploadKeystoreCommonName="Genesis" --androidUploadKeystoreOrganization="Chiubaka Technologies LLC" --androidUploadKeystoreCountry="US"',
+    'generate @chiubaka/nx-plugin:app.react-native --name=react-native-app --appName="Genesis React Native App" --appId="com.chiubaka.genesis.example.ReactNativeApp" --appleId="daniel@chiubaka.com" --appleDeveloperTeamId="AYCA644992" --androidEmulatorAvdName="Detox" --iosCodeSigningGitRepositoryUrl="git@github.com:chiubaka/ios-code-signing.git" --appStoreConnectKeyIssuerId="1ced7937-7d1e-4874-9bc9-823c1825148f" --appStoreConnectKeyId="43YKVHYVJQ" --androidUploadKeystoreCommonName="Genesis" --androidUploadKeystoreOrganization="Chiubaka Technologies LLC" --androidUploadKeystoreCountry="US"',
   );
 
   return workspace;
